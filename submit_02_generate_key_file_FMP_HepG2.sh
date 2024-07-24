9b6ef92766c864cb9fb4211c3bf0ae31df53b5c6
schmiedc <10744409+schmiedc@users.noreply.github.com>
2024-07-24 12:40:01 +0200

    Added modified submit scripts

---
+++ submit_02_generate_key_file_FMP_HepG2.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+#SBATCH --job-name=Generate_key_file
+#SBATCH --nodes=1
+
+# activate transfer-env
+source /euopen/screeningunit/Bioactives/transfer-env/bin/activate
+echo "Activated transfer-env"
+
+# get the directory where the script is executed from
+base_path="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+echo "Script directory: $base_path"
+
+# call python and execute script on base path
+/euopen/screeningunit/Bioactives/transfer-env/bin/python $base_path/02_generate_key_file_FMP_HepG2.py $base_path
+
+deactivate
---
+++ submit_05_create_load_data_Median_HepG2.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+#SBATCH --job-name=Generate_key_file
+#SBATCH --nodes=1
+
+# activate transfer-env
+source /euopen/screeningunit/Bioactives/transfer-env/bin/activate
+echo "Activated transfer-env"
+
+# get the directory where the script is executed from
+base_path="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+echo "Script directory: $base_path"
+
+# call python and execute script on base path
+/share/apps/python-3.9/bin/python3.9 $base_path/05_create_load_data_csv_Medina_HepG2.py $base_path
+
+# deactivate transfer-env
+deactivate
